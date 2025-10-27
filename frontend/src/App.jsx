5634b65ce5f56721252a4a190735ca62c3656586
nitin <nitin@onlinesportstech.com>
2025-10-27 11:35:07 +0530

    s

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 
 // Masters
 import PaperMaster from "./components/masters/PaperMaster";
@@ -13,49 +13,45 @@ import DieCuttingMaster from "./components/masters/DieCuttingMaster";
 
 // Customers / Orders / Reports
 import CustomerMaster from "./components/customers/CustomerMaster";
-import OrderEntry from "./OrderForm";
+import OrderForm from "./OrderForm";
 import Home from "./Home";
 import OrdersPage from "./OrderPage";
-import OrderForm from "./OrderForm";
 import AutoSheetCalculator from "./components/Autosheetcalculator";
-
-// Auth
-import LoginPage from "./components/Login";
-// import ProtectedRoute from "./components/ProtectedRoutes";
 import QuickOrderEntry from "./QuickOrderEntry";
 import FullOrderEntry from "./QuickOrderEntry";
 
+// Auth
+import LoginPage from "./components/Login";
+
 function App() {
   return (
-    <Router>
-      <Routes>
-        {/* Login */}
-        {/* <Route path="/" element={<LoginPage />} /> */}
+    <Routes>
+      {/* Login */}
+      {/* <Route path="/" element={<LoginPage />} /> */}
 
-        {/* Protected Routes */}
-        <Route path="/" element={<Home />} />
-        <Route path="/auto" element={<AutoSheetCalculator />} />
+      {/* Protected Routes */}
+      <Route path="/" element={<Home />} />
+      <Route path="/auto" element={<AutoSheetCalculator />} />
 
-        {/* Masters */}
-        <Route path="/masters/paper" element={<PaperMaster />} />
-        <Route path="/masters/printing" element={<PrintingMaster />} />
-        <Route path="/masters/lamination" element={<LaminationMaster />} />
-        <Route path="/masters/corrugation" element={<CorrugationMaster />} />
-        <Route path="/masters/pasting" element={<PastingMaster />} />
-        <Route path="/masters/transport" element={<TransportMaster />} />
-        <Route path="/masters/vendor" element={<VendorMaster />} />
-        <Route path="/die-cutting" element={<DieCuttingMaster />} />
+      {/* Masters */}
+      <Route path="/masters/paper" element={<PaperMaster />} />
+      <Route path="/masters/printing" element={<PrintingMaster />} />
+      <Route path="/masters/lamination" element={<LaminationMaster />} />
+      <Route path="/masters/corrugation" element={<CorrugationMaster />} />
+      <Route path="/masters/pasting" element={<PastingMaster />} />
+      <Route path="/masters/transport" element={<TransportMaster />} />
+      <Route path="/masters/vendor" element={<VendorMaster />} />
+      <Route path="/die-cutting" element={<DieCuttingMaster />} />
 
-        {/* Customers / Orders / Reports */}
-        <Route path="/customers" element={<CustomerMaster />} />
-        <Route path="/orders" element={<OrderForm />} />
-        <Route path="/orders-detail" element={<OrdersPage />} />
-        <Route path="/order-data-entry" element={<FullOrderEntry />} />
+      {/* Customers / Orders / Reports */}
+      <Route path="/customers" element={<CustomerMaster />} />
+      <Route path="/orders" element={<OrderForm />} />
+      <Route path="/orders-detail" element={<OrdersPage />} />
+      <Route path="/order-data-entry" element={<FullOrderEntry />} />
 
-        {/* Redirect unknown routes to home */}
-        {/* <Route path="*" element={<Navigate to="/" replace />} /> */}
-      </Routes>
-    </Router>
+      {/* Redirect unknown routes to home */}
+      <Route path="*" element={<Navigate to="/" replace />} />
+    </Routes>
   );
 }
 
